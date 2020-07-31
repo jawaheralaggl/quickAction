8b2b1b8d245103310168c855e83769e2d5e195b5
jawaheralaggl <jawaheralaggl@gmail.com>
2020-07-31 21:49:17 +0300

    Initial Commit

---
+++ quickAction/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  quickAction
+//
+//  Created by Jawaher Alagel on 7/31/20.
+//  Copyright © 2020 Jawaher Alaggl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
